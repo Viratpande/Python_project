908dd17be514c4bbb2ced875db9655bf5e09bce5
Nikhil Tiwari <142111838+Viratpande@users.noreply.github.com>
2023-09-02 22:26:44 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,16 @@
+
+a = float(input("Enter the value of 'a': "))
+b = float(input("Enter the value of 'b': "))
+
+floor_division_result = a // b
+
+
+power_result = a ** b
+
+
+are_equal = a == b
+
+
+print("1. Floor Division result:", floor_division_result)
+print("2. 'a' raised to the power 'b' result:", power_result)
+print("3. Are 'a' and 'b' equal?", are_equal)
